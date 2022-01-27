7ee44da3b7c43442e88ab893cef087b77d9c2b2e
reesericci <me@reeseric.ci>
2022-01-27 09:21:44 -0600

    ggggrrrrraaaappphhhhqqqqqlllllll!

--- index.ts
+++ index.ts
@@ -2,6 +2,24 @@
 import { Datepicker } from 'vanillajs-datepicker';
 import Alpine from 'alpinejs'
 import AlpineInstance from 'alpinejs';
+import { request, gql, RequestDocument } from 'graphql-request'
+
+interface DayQueryInterface {
+    query: RequestDocument
+}
+class DayQuery implements DayQueryInterface {
+    query: RequestDocument;
+    constructor(date: Date) {
+        this.query = gql`
+        {
+            getDay(day: ${date.getDate()}, month: ${date.getMonth() + 1}, year: ${date.getFullYear()}) {
+            type
+            }
+        }
+        `
+    }
+}
+
 declare global {
     interface Window {
        Alpine: typeof AlpineInstance
@@ -10,6 +28,15 @@ declare global {
 
 window.Alpine = Alpine
  
+
+Alpine.store("functions", {
+    genDayQuery(date: Date) {return new DayQuery(date)},
+    async fetchDay(query: DayQueryInterface) { 
+        const req = await request("https://api.purplegrey.today/.netlify/functions/graphql", query.query) 
+        return req;
+    }
+})
+
 Alpine.start()
 
 const datepicker = new Datepicker(document.getElementById("calendar").childNodes[1], {
