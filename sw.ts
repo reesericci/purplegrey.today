4a2eb63b3f24ccba109c76eccf466648ef88c340
reesericci <me@reeseric.ci>
2022-01-28 07:31:37 -0600

    pbgs?

--- sw.ts
+++ sw.ts
@@ -1,8 +1,29 @@
 import {manifest, version} from '@parcel/service-worker';
 
+
+async function registerPeriodicSync() {
+  const registration = await navigator.serviceWorker.ready;
+  try {
+    await registration.periodicSync.register('update-day', {
+      minInterval: 1 * 60 * 60 * 1000,
+    });
+  } catch {
+    console.log('Periodic Sync could not be registered!');
+  }
+}
+
+addEventListener('periodicsync', event => {
+  if (event.tag == 'update-day') {
+    event.waitUntil(() => {
+      dispatchEvent(new Event("picked"))
+    });
+  }
+});
+
 async function install() {
   const cache = await caches.open(version);
   await cache.addAll(manifest);
+  await registerPeriodicSync()
 }
 addEventListener('install', e => e.waitUntil(install()));
 
